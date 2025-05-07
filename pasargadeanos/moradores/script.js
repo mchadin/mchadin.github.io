272d1d3d3a394b7ef825c5350cacc5fa72003b0d
Machado <machadomatheusmagalhaes@gmail.com>
2025-05-07 19:43:18 -0300

    Atualização Card Moradores

--- pasargadeanos/moradores/script.js
+++ pasargadeanos/moradores/script.js
@@ -4,26 +4,50 @@ function criarCards() {
   moradores.forEach((aluno) => {
     const cardHTML = `
           <div class="col-6 col-sm-6 col-md-4 col-lg-3 my-3">
-              <div class="card">
-                  <img src="fotos/${aluno.codigo.padStart(3, "0")}.png" 
-                       class="card-img-top h-md-525 h-sm-200"
-                       alt="${aluno.apelido}">
-                  <div class="card-body">
-                      <h5 class="card-title text-success text-center fw-bold">${
-                        aluno.apelido
-                      }</h5>
-                      <p class="card-text text-center">${aluno.nome}</p>
-                      <p class="card-text text-center">${aluno.curso} – ${
+    <div class="card">
+        <img src="fotos/${aluno.codigo.padStart(3, "0")}.png" 
+             class="card-img-top h-md-525 h-sm-200"
+             alt="${aluno.apelido}">
+        <div class="card-body">
+            <h5 class="card-title text-success text-center fw-bold">${
+              aluno.apelido
+            }</h5>
+            <p class="card-text text-center">${aluno.nome}</p>
+            <p class="card-text text-center">${aluno.curso} – ${
       aluno.periodo
     }</p>
-                      ${
-                        aluno.cidade
-                          ? `<p class="card-text text-center text-muted small">${aluno.cidade} - ${aluno.estado}</p>`
-                          : ""
-                      }
-                  </div>
-              </div>
-          </div>
+            ${
+              aluno.cidade
+                ? `<p class="card-text text-center text-muted small">${aluno.cidade} - ${aluno.estado}</p>`
+                : ""
+            }
+        </div>
+        <!-- Ícones com espaçamento horizontal -->
+        <ul class="list-unstyled d-flex justify-content-center mb-3">
+            <li class="mx-2">
+                <a href="mailto:${
+                  aluno.email
+                }" class="text-decoration-none text-dark">
+                    <i class="bi bi-envelope"></i>
+                </a>
+            </li>
+            <li class="mx-2">
+                <a href="${
+                  aluno.linkedin
+                }" target="_blank" class="text-decoration-none text-dark">
+                    <i class="bi bi-linkedin"></i>
+                </a>
+            </li>
+            <li class="mx-2">
+                <a href="https://instagram.com/${
+                  aluno.instagram
+                }" target="_blank" class="text-decoration-none text-dark">
+                    <i class="bi bi-instagram"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
       `;
     container.innerHTML += cardHTML;
   });
